96bba37c7efd4d7b18e6b71f5878120070b3fc39
alexsolanki2 <46710296+alexsolanki2@users.noreply.github.com>
2021-04-08 23:59:38 +0530

    =fourth

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -62,7 +62,7 @@ const User = ({ match }) => {
                 <p>{bio}</p>
               </Fragment>
             )}
-            <a href={html_url} className='btn btn-dark my-1' target="_blank">
+            <a href={html_url} className='btn btn-dark my-1'>
               Visit Github Profile
             </a>
             <ul>
